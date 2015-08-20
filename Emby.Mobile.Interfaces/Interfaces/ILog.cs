0de494120ac8c87a73903db447c8684f3c0a9c29
Scott Lovegrove <scottisafool@live.co.uk>
2015-08-20 08:00:10 +0100

    Added Interfaces project

---
+++ Emby.Mobile.Interfaces/Interfaces/ILog.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Emby.Mobile.Interfaces
+{
+    public interface ILog
+    {
+        void Info(string message, params object[] parameters);
+        void InfoException(string message, Exception exception);
+        void Warning(string message, params object[] parameters);
+        void WarningException(string message, Exception exception);
+        void Error(string message, params object[] parameters);
+        void ErrorException(string message, Exception exception);
+        void Fatal(string message, params object[] parameters);
+        void FatalException(string message, Exception exception);
+        void Debug(string message, params object[] parameters);
+        void DebugException(string message, Exception exception);
+        void Trace(string message, params object[] parameters);
+        void TraceException(string message, Exception exception);
+    }
+}
